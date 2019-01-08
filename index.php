00d1c41eb4dd5ec0dd9369f9b0381a24f7a1d9aa
Jamerson Souza <jamersonsouza@outlook.com>
2019-01-08 15:10:23 -0300

    add arquivo index do projeto nix

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="author" content="Jamerson Souza">
+        <meta name="application-name" content="Nix Studio">
+        <meta name="description" content="Nix é o seu Portal de Estudos e Leitura Online.">
+        <meta name="keywords" content="Biblioteca, Cursos EaD, Apostilas, Materiais Digitais.">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+        <title>:: Nix ::</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Roboto" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">  
+    </head>
+    <header id="cbc">
+        <a href="#"><img src="_imagens/logo_nix.png" alt="Nix - Portal de Estudos Online"></a>
+        <nav id="menu">
+        <ul>
+            <li><a href="login/pg-login.php">ENTRAR</a></li>
+            <li><a href="cadastro/cad-usuario.php">CADASTRE-SE</a></li>
+            </ul>
+        </nav>        
+    </header>
+    <body>
+    <section id="hero">
+        <h1>CONHEÇA O PROJETO <strong>NIX</strong></h1>
+            <h3>Agora ficou fácil <strong>encontrar</strong> um ótimo lugar para <strong>estudar!</strong></h3>
+        <br>
+            <h2><a href="#">SAIBA MAIS!</a></h2>
+        </section>
+        
+        <section id="portal">
+            
+            <h1 class="titu">CONTEÚDOS QUE VOCÊ SÓ ENCONTRA NO <strong>NIX!</strong></h1>
+            
+            <!-- CARDS DE APRESENTAÇÃO -->
+            
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="_imagens/icone-apostila.png" alt="Apostilas Nix - Baixe">
+  <div class="card-body">
+    <h5 class="card-title">APOSTILAS</h5>
+    <p class="card-text">BAIXE APOSTILAS <strong>GRATUITAMENTE NO NIX</strong>, ALÉM DE PODER DISPONIBILIZAR NO PORTAL SUAS PRÓPRIAS APOSTILAS</p>
+    <a href="#" class="btn btn-primary">Saiba Mais</a>
+  </div>
+</div>
+      
+            <!-- 2º CARD CURSOS -->
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="_imagens/icone-curso.png" alt="Apostilas Nix - Baixe">
+  <div class="card-body">
+    <h5 class="card-title">CURSOS INTERATIVOS</h5>
+    <p class="card-text"><strong>NIX DISPONIBILIZA</strong> TREINAMENTOS DE TERCEIROS COM <strong>TODOS DIREITOS RESERVADOS!</strong> AGORA FICOU FÁCIL ESTUDAR.</p>
+    <a href="#" class="btn btn-primary">Saiba Mais</a>
+  </div>
+</div>   
+            
+            <!-- 3º CARD BIBLIOTECA NIX -->
+            
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="_imagens/icone-bibliot.png" alt="Apostilas Nix - Baixe">
+  <div class="card-body">
+    <h5 class="card-title">BIBLIOTECA NIX!</h5>
+    <p class="card-text">A BIBLIOTECA <strong>ONLINE DO NIX  </strong> VOCÊ PODE BAIXAR E <strong>COMPARTILHAR LIVROS</strong> COM A COMUNIDADE.</p>
+      <br>
+    <a href="#" class="btn btn-primary">Saiba Mais</a>
+  </div>
+</div>
+    <!-- GAMBIARRA PARA O TEXTO DA SECTION ABAIXO NÃO BUGAR -->
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </section>
+    <section class="portal-rodape">
+        <h3>
+            REALIZE SEU <strong>CADASTRO</strong> E DESFRUTE DE <br>TODOS OS <strong>SERVIÇOS GRATUITOS!</strong>
+        
+        </h3>
+         
+        </section>
+       <hr>
+        <section class="como-funciona">
+        <h5>COMO FUNCIONA O NIX?</h5>
+            <p>Nix é o seu portal de estudos, disponível com cursos e biblioteca online. Além de materiais didáticos compartilhados na comunidade pelos próprios usuários.</p>
+            <hr>
+            <ul class="grid">
+            <li>
+                <i class="fas fa-user-plus"></i>
+                <h4>CADASTRO</h4>
+                <p>Ao realizar seu Cadastro você terá acesso exclusivo aos conteúdos da comunidade. Podendo também compartilhar seu conteúdo em nosso Site.</p>
+                </li>
+                <li>
+                    <i class="fas fa-video"></i>
+                    <h4>OS CURSOS SÃO CRIADOS PELO NIX?</h4>
+                    <p>Nosso portal contém diversas categorias de cursos, criados por terceiros. Lembrando sempre de deixar todos direitos reservados aos seus criadores.</p>
+                </li>
+                <li>
+                    <i class="fas fa-question-circle"></i>
+                    <h4>SOBRE O NIX</h4>
+                    <p>A missão do NIX é selecionar e compartilhar com os usuários os melhores materias de estudos, não apenas de uma área especifica mais englobando diversas disciplinas.</p>
+                </li>
+            </ul>        
+        </section>
+        
+        <section class="newletters">
+            <h1>NEWSLETTER</h1>
+        <p>RECEBA SEMPRE NOVIDADES POR E-MAIL <br> CADASTRE-SE AGORA MESMO!</p>
+            <hr>
+            <form>
+            <input type="text" name="email" placeholder="INSIRA SEU EMAIL">
+                <button type="submit">CADASTRAR!</button>
+                   </form>
+        </section>
+        
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
